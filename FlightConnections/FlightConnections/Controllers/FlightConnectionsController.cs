da080246db59ba6e14e17eb7bcb25e983eb0e6bd
Cleber2504 <santana.cleber@hotmail.com>
2025-01-09 14:44:04 -0300

    Meu primeiro Git

---
+++ FlightConnections/FlightConnections/Controllers/FlightConnectionsController.cs
@@ -0,0 +1,41 @@
+﻿using FlightConnections.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace FlightConnections.Controllers
+{
+    public class FlightConnectionsController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<ActionResult> Post(string nome) { return Ok();}
+
+        [HttpGet("{origin}/{destiny}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<ActionResult> Get(){ return Ok(); }
+
+        [HttpGet("{Id}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<ActionResult> Get(int id) { 
+            FlightRoutes routes = new FlightRoutes();
+            return Ok();
+        }
+
+        [HttpDelete("Id")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<ActionResult> Delete(int id) { return Ok(); }
+
+        [HttpPut("Id")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult> Put(int id) { return Ok(); }
+
+
+
+
+    }
+}
---
+++ FlightConnections/FlightConnections/Program.cs
@@ -0,0 +1,18 @@
+using FlightConnections;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
+}
+
+
